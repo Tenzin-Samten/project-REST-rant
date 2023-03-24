e39fb467ab0311dc21a4bb3f766991dcd64d5abd
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-03-24 14:27:23 -0400

    places

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,21 @@ const router = require('express').Router()
 
 // GET /places
 router.get('/', (req, res) => {
-    res.render('GET/places')
+    let places = [{
+        name: 'TIBET',
+        city: 'QUEENS',
+        state: 'NY',
+        cuisines: 'TIBETAN',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'LHASA CAFE',
+        city: 'BROOKLYN',
+        state: 'NY',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('GET/places', {places})
   })
 
   router.post('/places', (req, res) => {
