2610855ecfc0cd55536bc7bf37ee8ca45b5bdd5d
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-03-29 14:09:25 -0400

    Addong Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get('/', (req, res) => {
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.png'
+    pic: '/images/resturant.jpg'
   }, {
       name: 'Coding Cat Cafe',
       city: 'Phoenix',
       state: 'AZ',
       cuisines: 'Coffee, Bakery',
-      pic: '/images/coffee-cat.jpg'
+      pic: '/images/cafe.jpg'
   }]
   
       
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,5 +1,8 @@
+const React = require('react')
+const Def = require('../default')
+
 function index (data) {
-  let placesFormatted = data.places.map((place) => {
+	let placesFormatted = data.places.map((place) => {
     return (
       <div className="col-sm-6">
         <h2>{place.name}</h2>
@@ -8,22 +11,22 @@ function index (data) {
         </p>
         <img src={place.pic} alt={place.name} />
         <p className="text-center">
-          Located in {place.city}, {place.state}
+          Located in {Located in Seattle}, {WA}
         </p>
       </div>
     )
   })
-  return (
-    <Def>
-        <main>
-            <h1>Places to Rant or Rave About</h1>
-            <div className="row">
-              {placesFormatted}
-            </div>
-        </main>
-    </Def>
-  )
-}
+  
+	return (
+	  <Def>
+	      <main>
+		  <h1>Places to Rant or Rave About</h1>
+		  <div className="row">
+		    {placesFormatted}
+		  </div>
+	      </main>
+	  </Def>
+	)
+      }
 
-  
-  
+module.exports = index
