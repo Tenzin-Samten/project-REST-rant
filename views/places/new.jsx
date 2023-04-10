af43ed2f2c618f968abddd35044bafc69272017d
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-04-10 17:41:19 -0400

    part9

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,17 +1,22 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
+function new_form(data) {
 	return (
 		<Def>
 			<main>
 				<h1>Add a New Place</h1>
 				<form method="POST" action="/places">
-				<div className="form-group">
-  			<label for="founded">Founded Year</label>
-  			<input className="form-control" id="founded" name="founded" />
-				</div>
-					<div className="form-group">
+				<div className="form-group col-sm-4">
+  <label htmlFor="founded">Founded Year</label>
+  <input 
+    type="number" 
+    className="form-control" 
+    id="founded" 
+    name="founded" 
+    value={new Date().getFullYear()} />
+</div>
+<div className="form-group">
 						<label htmlFor="name">Place Name</label>
 						<input className="form-control" id="name" name="name" required />
 					</div>
--- views/show.jsx
+++ views/show.jsx
@@ -2,35 +2,34 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <a href="" className="btn btn-warning"> 
-  Edit
-</a>  
-<form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-  <div className="form-group">
-      <label htmlFor="name">Place Name</label>
-      <input 
-        className="form-control" 
-        id="name" 
-        name="name" 
-        value={data.place.name} 
-      required />
-  </div>
-  ...
-</form>
-
-
-
-
-
-
-          </main>
-        </Def>
+    return (<Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+          <h1>{ data.place.name }</h1>
+            <h2>
+              Rating
+              </h2>
+              {rating}
+              <br />
+              <h2>
+              Description
+            </h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+          </div>
+      </main>
+    </Def>
     )
-}
-
-module.exports = show
+    module.exports = show
 
